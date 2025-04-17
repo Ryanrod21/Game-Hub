7b0f1b76217a6e73be2d6dff7491571cef730ac7
Ryan <ryancode2321@gmail.com>
2025-04-17 15:07:19 -0500

    making + work

--- src/sections/FriendsList.jsx
+++ src/sections/FriendsList.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react"
 import FriendsCard from "../components/FriendsCard"
+import '../App.css'
 
 function FriendsList ({ProfileData}) {
-    return <div><p>Online Players +</p>
-        <FriendsCard ProfileData={ProfileData} />
+    const [showPopup, setPopup] = useState(false)
+    const [inputValue, setInputValue] = useState("");
+
+    const togglePopup = () => setPopup(!showPopup)
+
+    const handleInputChange = (e) => setInputValue(e.target.value)
+
+
+    return  <>
+    
+    <div className="FriendsPanel"><p>Online Players</p>
+        <button className="addBtn" onClick={togglePopup}>+</button>
+        {showPopup && (
+        <div className="popup-box">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Type something..."
+          />
+        </div>
+      )}
     </div>
+        <FriendsCard ProfileData={ProfileData} />
+    </>
 }
 
 export default FriendsList
